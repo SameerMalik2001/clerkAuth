d1e998b0655e9e728094484d4ead8ece65d9f25f
Sameer Malik <sameer7417277576@gmail.com>
2025-03-16 16:59:00 +0530

    some fix

--- src/app/components/MyTableBody.tsx
+++ src/app/components/MyTableBody.tsx
@@ -1,4 +1,5 @@
-"use client";
+"use client"
+
 import { TableBody, TableCell } from "@/components/ui/table";
 import { getTodos, Todo } from "@/lib/api";
 import { formatDate } from "@/lib/utils";
@@ -28,21 +29,31 @@ const MyTableBody = ({ cui }: { cui: string }) => {
   }, [tableRefresh]);
 
   useEffect(() => {
-    if(checkedAllTodo && data) {
+    if (checkedAllTodo && data) {
       updateCheckedAllTodoIds(data?.map(todo => todo.id));
     }
   }, [checkedAllTodo])
 
   return (
-    <TableBody >
-      {data?.map((row: Todo) => (
-        <BodyWrapper row={row} key={row.id}>
-          <TableCell className="capitalize">{row.title}</TableCell>
-          <TableCell className={`${row.completed === 'Completed' ? 'text-green-600' : row.completed === 'In Progress' ? 'text-orange-600' : 'text-blue-600' }`} >{row.completed}</TableCell>
-          <TableCell>{formatDate(row.createdAt)}</TableCell>
-          <TableCell>{formatDate(row.updatedAt)}</TableCell>
-        </BodyWrapper>
-      ))}
+    <TableBody>
+      {data && data.length > 0 ? (
+        data.map((row: Todo) => (
+          <BodyWrapper row={row} key={row.id}>
+            <TableCell className="capitalize">{row.title}</TableCell>
+            <TableCell className={row.completed === 'Completed' ? 'text-green-600' : row.completed === 'In Progress' ? 'text-orange-600' : 'text-blue-600'}>
+              {row.completed}
+            </TableCell>
+            <TableCell>{formatDate(row.createdAt)}</TableCell>
+            <TableCell>{formatDate(row.updatedAt)}</TableCell>
+          </BodyWrapper>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={4} className="text-center text-gray-500 p-4">
+            No records found
+          </td>
+        </tr>
+      )}
     </TableBody>
   );
 };
